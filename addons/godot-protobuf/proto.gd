3d4dee985ef45a16aeffaf29b5620c8c72b629b4
Derrik Milligan <derrik.milligan.dev@gmail.com>
2024-04-23 16:22:08 -0600

    Got some work done on ProtobufField and a base ProtobufMessage

--- addons/godot-protobuf/proto.gd
+++ addons/godot-protobuf/proto.gd
@@ -58,29 +58,98 @@ const WIRE_TYPE_LOOKUP = {
 	DATA_TYPE.MAP:      WIRE_TYPE.LENGTHDEL,
 }
 
-class ProtobufEncoder:
-	static func binary_representation(number: int) -> String:
-		var binary_str = ""
-		var num = number
+class ProtobufField:
+	var name     : String
+	var position : int
+	var data_type: DATA_TYPE
+	var value
 
-		# Use a loop to divide the number by 2 and collect remainders
-		while num > 0:
-			# Collect the remainder (0 or 1) and prepend it to binary_str
-			binary_str = str(num % 2) + binary_str
-			# Update the number by dividing it by 2
-			num /= 2
+	func _init(_name: String, _position: int, _data_type: DATA_TYPE):
+		name      = _name
+		position  = _position
+		data_type = _data_type
 
-		# Return the binary string
-		return binary_str
+	func encode() -> PackedByteArray:
+		return ProtobufEncoder.encode_field(self)
 
-	static func encode_varint(value, data_type: DATA_TYPE) -> PackedByteArray:
+class ProtobufMessage:
+	var fields: Dictionary = {}
+
+	func _init(initial_data: Dictionary = {}):
+		_init_fields()
+
+		for key in initial_data.keys():
+			if fields.has(key):
+				fields[key].value = initial_data[key]
+
+	## To be overriden by each message to prep it's fields
+	func _init_fields():
+		pass
+
+	func add_field(name: String, position: int, data_type: DATA_TYPE):
+		var field = ProtobufField.new(name, position, data_type)
+		fields[name] = field
+
+	func encode() -> PackedByteArray:
 		var bytes: PackedByteArray = PackedByteArray()
 
-		if typeof(value) == TYPE_BOOL:
+		for field in fields.values():
+			if field.value != null:
+				bytes.append_array(field.encode())
+
+		return bytes
+
+	# func decode(bytes: PackedByteArray):
+	# 	var position = 0
+	# 	while position < bytes.size():
+	# 		var byte = bytes[position]
+	# 		var wire_type = byte & 0x07
+	# 		var field_number = byte >> 3
+
+	# 		var field = fields.values().find(lambda f: f.position == field_number)
+	# 		if field == null:
+	# 			position += 1
+	# 			continue
+
+	# 		match wire_type:
+	# 			WIRE_TYPE.VARINT:
+	# 				var varint = ProtobufEncoder.decode_varint(bytes[position+1:])
+	# 				field.value = varint
+	# 				position += 1 + varint.size()
+	# 			WIRE_TYPE.FIX32:
+	# 				var fix32 = bytes[position+1:position+5]
+	# 				var spb = StreamPeerBuffer.new()
+	# 				spb.put_data_array(fix32)
+	# 				field.value = spb.get_float()
+	# 				position += 5
+	# 			WIRE_TYPE.FIX64:
+	# 				var fix64 = bytes[position+1:position+9]
+	# 				var spb = StreamPeerBuffer.new()
+	# 				spb.put_data_array(fix64)
+	# 				field.value = spb.get_double()
+	# 				position += 9
+	# 			WIRE_TYPE.LENGTHDEL:
+	# 				var length = ProtobufEncoder.decode_varint(bytes[position+1:])
+	# 				var data = bytes[position+1+length:position+1+length+length]
+	# 				var spb = StreamPeerBuffer.new()
+	# 				spb.put_data_array(data)
+	# 				field.value = spb.get_data_array()
+	# 				position += 1 + length + length
+	# 			_:
+	# 				assert(false, "Unsupported wire type")
+
+
+class ProtobufEncoder:
+
+	static func encode_varint(field: ProtobufField) -> PackedByteArray:
+		var bytes: PackedByteArray = PackedByteArray()
+		var value = field.value
+
+		if typeof(field.value) == TYPE_BOOL:
 			value = 1 if value else 0
 
 		# If the value is negative, convert it to a positive number
-		if data_type == DATA_TYPE.SINT32 || data_type == DATA_TYPE.SINT64:
+		if field.data_type == DATA_TYPE.SINT32 || field.data_type == DATA_TYPE.SINT64:
 			if value < -2147483648:
 				value = (value << 1) ^ (value >> 63)
 			else:
@@ -113,36 +182,44 @@ class ProtobufEncoder:
 
 		return value
 
-	static func encode_bytes(value, data_type: DATA_TYPE, byte_count: int) -> PackedByteArray:
-		if data_type == DATA_TYPE.FLOAT:
+	static func encode_bytes(field: ProtobufField, byte_count: int) -> PackedByteArray:
+		if field.data_type == DATA_TYPE.FLOAT:
 			var spb = StreamPeerBuffer.new()
-			spb.put_float(value)
+			spb.put_float(field.value)
 			return spb.get_data_array()
 		
-		if data_type == DATA_TYPE.DOUBLE:
+		if field.data_type == DATA_TYPE.DOUBLE:
 			var spb = StreamPeerBuffer.new()
-			spb.put_double(value)
+			spb.put_double(field.value)
 			return spb.get_data_array()
+
+		if field.data_type == DATA_TYPE.FIXED32 or field.data_type == DATA_TYPE.FIXED64:
+			assert(field.value >= 0, "Fixed types can't be negative, use SFIXED instead")
 		
 		var bytes : PackedByteArray = PackedByteArray()
 
+		var value = field.value
 		for i in range(byte_count):
 			bytes.append(value & 0xFF)
 			value >>= 8
 
 		return bytes
 
-	static func encode_field(value, data_type: DATA_TYPE):
-		var wire_type = WIRE_TYPE_LOOKUP[data_type]
+	static func encode_field(field: ProtobufField) -> PackedByteArray:
+		var bytes: PackedByteArray = PackedByteArray()
+		var wire_type = WIRE_TYPE_LOOKUP[field.data_type]
 
-		if wire_type == WIRE_TYPE.VARINT:
-			return encode_varint(value, data_type)
+		bytes.append((field.position << 3) | wire_type)
 
-		if wire_type == WIRE_TYPE.FIX32:
-			return encode_bytes(value, data_type, 4)
+		match wire_type:
+			WIRE_TYPE.VARINT:
+				bytes.append_array(encode_varint(field))
+			WIRE_TYPE.FIX32:
+				bytes.append_array(encode_bytes(field, 4))
+			WIRE_TYPE.FIX64:
+				bytes.append_array(encode_bytes(field, 8))
+			_:
+				assert(false, "Unsupported wire type")
 
-		if wire_type == WIRE_TYPE.FIX64:
-			return encode_bytes(value, data_type, 8)
-
-
+		return bytes
 
--- test.gd
+++ test.gd
@@ -6,73 +6,117 @@ extends SceneTree
 
 const proto = preload("res://addons/godot-protobuf/proto.gd")
 
+class TestMessage extends proto.ProtobufMessage:
+  func _init_fields():
+    add_field("int32", 1, proto.DATA_TYPE.INT32)
+    add_field("int64", 2, proto.DATA_TYPE.INT64)
+    add_field("float", 3, proto.DATA_TYPE.FLOAT)
+    add_field("double", 4, proto.DATA_TYPE.DOUBLE)
+    add_field("uint32", 5, proto.DATA_TYPE.UINT32)
+    add_field("uint64", 6, proto.DATA_TYPE.UINT64)
+    add_field("sint32", 7, proto.DATA_TYPE.SINT32)
+    add_field("sint64", 8, proto.DATA_TYPE.SINT64)
+    add_field("fixed32", 9, proto.DATA_TYPE.FIXED32)
+    add_field("fixed64", 10, proto.DATA_TYPE.FIXED64)
+    add_field("sfixed32", 11, proto.DATA_TYPE.SFIXED32)
+    add_field("sfixed64", 12, proto.DATA_TYPE.SFIXED64)
+    add_field("bytes", 14, proto.DATA_TYPE.BYTES)
+    add_field("bool", 15, proto.DATA_TYPE.BOOL)
+
+  func set_int32(value):
+    fields["int32"].value = value;
+
+  func set_int64(value):
+    fields["int64"].value = value;
+
+  func set_uint32(value):
+    fields["uint32"].value = value;
+
+  func set_uint64(value):
+    fields["uint64"].value = value;
+
+  func set_sint32(value):
+    fields["sint32"].value = value;
+
+  func set_sint64(value):
+    fields["sint64"].value = value;
+
+  func set_fixed32(value):
+    fields["fixed32"].value = value;
+  
+  func set_fixed64(value):
+    fields["fixed64"].value = value;
+  
+  func set_sfixed32(value):
+    fields["sfixed32"].value = value;
+  
+  func set_sfixed64(value):
+    fields["sfixed64"].value = value;
+  
+  func set_bool(value):
+    fields["bool"].value = value;
+
+  func get_int32(value):
+    return fields["int32"].value;
+
+  func get_int64(value):
+    return fields["int64"].value;
+
+  func get_uint32(value):
+    return fields["uint32"].value;
+
+  func get_uint64(value):
+    return fields["uint64"].value;
+
+  func get_sint32(value):
+    return fields["sint32"].value;
+
+  func get_sint64(value):
+    return fields["sint64"].value;
+
+  func get_fixed32(value):
+    return fields["fixed32"].value;
+  
+  func get_fixed64(value):
+    return fields["fixed64"].value;
+  
+  func get_sfixed32(value):
+    return fields["sfixed32"].value;
+  
+  func get_sfixed64(value):
+    return fields["sfixed64"].value;
+  
+  func get_bool(value):
+    return fields["bool"].value;
+
 class TestProtobufEncoder:
   static func run_tests():
-    TestProtobufEncoder.test_encode_field()
+    TestProtobufEncoder.test_test_message()
 
-  # static func test_encode_varint():
-  #   var encoded = proto.ProtobufEncoder.encode_varint(1000)
-  #   assert(encoded == PackedByteArray([0xE8, 0x07]))
+  static func test_test_message():
+    assert(TestMessage.new({ "int32": 123 }).encode() == PackedByteArray([0x08, 0x7B]))
+    assert(TestMessage.new({ "int32": -123 }).encode() == PackedByteArray([0x08, 0x85, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01]))
+    assert(TestMessage.new({ "int64": 123 }).encode() == PackedByteArray([0x10, 0x7B]))
+    assert(TestMessage.new({ "int64": -123 }).encode() == PackedByteArray([0x10, 0x85, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01]))
+    assert(TestMessage.new({ "float": 123.456 }).encode() == PackedByteArray([0x1D, 0x79, 0xE9, 0xF6, 0x42]))
+    assert(TestMessage.new({ "float": -123.456 }).encode() == PackedByteArray([0x1D, 0x79, 0xE9, 0xF6, 0xC2]))
+    assert(TestMessage.new({ "double": 123.456 }).encode() == PackedByteArray([0x21, 0x77, 0xBE, 0x9F, 0x1A, 0x2F, 0xDD, 0x5E, 0x40]))
+    assert(TestMessage.new({ "double": -123.456 }).encode() == PackedByteArray([0x21, 0x77, 0xBE, 0x9F, 0x1A, 0x2F, 0xDD, 0x5E, 0xC0]))
+    assert(TestMessage.new({ "uint32": 123 }).encode() == PackedByteArray([0x28, 0x7B]))
+    assert(TestMessage.new({ "uint64": 123 }).encode() == PackedByteArray([0x30, 0x7B]))
+    assert(TestMessage.new({ "sint32": 123 }).encode() == PackedByteArray([0x38, 0xF6, 0x01]))
+    assert(TestMessage.new({ "sint32": -123 }).encode() == PackedByteArray([0x38, 0xF5, 0x01]))
+    assert(TestMessage.new({ "sint64": 123 }).encode() == PackedByteArray([0x40, 0xF6, 0x01]))
+    assert(TestMessage.new({ "sint64": -123 }).encode() == PackedByteArray([0x40, 0xF5, 0x01]))
+    assert(TestMessage.new({ "fixed32": 123 }).encode() == PackedByteArray([0x4D, 0x7B, 0x00, 0x00, 0x00]))
+    assert(TestMessage.new({ "sfixed32": 123 }).encode() == PackedByteArray([0x5D, 0x7B, 0x00, 0x00, 0x00]))
+    assert(TestMessage.new({ "sfixed32": -123 }).encode() == PackedByteArray([0x5D, 0x85, 0xFF, 0xFF, 0xFF]))
+    assert(TestMessage.new({ "fixed64": 123 }).encode() == PackedByteArray([0x51, 0x7B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]))
+    assert(TestMessage.new({ "sfixed64": 123 }).encode() == PackedByteArray([0x61, 0x7B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]))
+    assert(TestMessage.new({ "sfixed64": -123 }).encode() == PackedByteArray([0x61, 0x85, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]))
+    # assert(TestMessage.new({ "bytes": PackedByteArray([0x01, 0x02, 0x03]) }).encode() == PackedByteArray([0x4D, 0x7B, 0x00, 0x00, 0x00]))
 
-  #   encoded = proto.ProtobufEncoder.encode_varint(150)
-  #   assert(encoded == PackedByteArray([0x96, 0x01]))
-
-  #   encoded = proto.ProtobufEncoder.encode_varint(123456789)
-  #   assert(encoded == PackedByteArray([0x95, 0x9A, 0xEF, 0x3A]))
-
-  # static func test_decode_varint():
-  #   var decoded = proto.ProtobufEncoder.decode_varint(PackedByteArray([0xE8, 0x07]))
-  #   assert(decoded == 1000)
-
-  #   decoded = proto.ProtobufEncoder.decode_varint(PackedByteArray([0x96, 0x01]))
-  #   assert(decoded == 150)
-
-  #   decoded = proto.ProtobufEncoder.decode_varint(PackedByteArray([0x95, 0x9A, 0xEF, 0x3A]))
-  #   assert(decoded == 123456789)
-
-  static func test_encode_field():
-    var encoded = proto.ProtobufEncoder.encode_field(123.456, proto.DATA_TYPE.FLOAT)
-    assert(encoded == PackedByteArray([0x79, 0xE9, 0xF6, 0x42]))
-
-    encoded = proto.ProtobufEncoder.encode_field(123.456, proto.DATA_TYPE.DOUBLE)
-    assert(encoded == PackedByteArray([0x77, 0xBE, 0x9F, 0x1A, 0x2F, 0xDD, 0x5E, 0x40]))
-
-    encoded = proto.ProtobufEncoder.encode_field(123, proto.DATA_TYPE.INT32)
-    assert(encoded == PackedByteArray([0x7B]))
-
-    encoded = proto.ProtobufEncoder.encode_field(-123, proto.DATA_TYPE.INT32)
-    assert(encoded == PackedByteArray([0x85, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01]))
-
-    encoded = proto.ProtobufEncoder.encode_field(123, proto.DATA_TYPE.UINT32)
-    assert(encoded == PackedByteArray([0x7B]))
-
-    encoded = proto.ProtobufEncoder.encode_field(123, proto.DATA_TYPE.FIXED32)
-    assert(encoded == PackedByteArray([0x7B, 0x00, 0x00, 0x00]))
-
-    encoded = proto.ProtobufEncoder.encode_field(-123, proto.DATA_TYPE.SFIXED32)
-    assert(encoded == PackedByteArray([0x85, 0xFF, 0xFF, 0xFF]))
-
-    encoded = proto.ProtobufEncoder.encode_field(123, proto.DATA_TYPE.INT64)
-    assert(encoded == PackedByteArray([0x7B]))
-
-    encoded = proto.ProtobufEncoder.encode_field(-123, proto.DATA_TYPE.INT64)
-    assert(encoded == PackedByteArray([0x85, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01]))
-
-    encoded = proto.ProtobufEncoder.encode_field(123, proto.DATA_TYPE.UINT64)
-    assert(encoded == PackedByteArray([0x7B]))
-
-    encoded = proto.ProtobufEncoder.encode_field(123, proto.DATA_TYPE.FIXED64)
-    assert(encoded == PackedByteArray([0x7B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]))
-
-    encoded = proto.ProtobufEncoder.encode_field(-123, proto.DATA_TYPE.SFIXED64)
-    assert(encoded == PackedByteArray([0x85, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]))
-
-    encoded = proto.ProtobufEncoder.encode_field(true, proto.DATA_TYPE.BOOL)
-    assert(encoded == PackedByteArray([1]))
-
-    encoded = proto.ProtobufEncoder.encode_field(false, proto.DATA_TYPE.BOOL)
-    assert(encoded == PackedByteArray([0]))
-
+    assert(TestMessage.new({ "int32": 123, "int64": 456, "fixed64": 789 }).encode() == PackedByteArray([0x08, 0x7B, 0x10, 0xC8, 0x03, 0x51, 0x15, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]))
 
 
 func _init():
